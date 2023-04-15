9ccd6fbe526abc333b2a7fdb4b4625d19303b0b7
Kiley Zytka <kiley.zytka@gmail.com>
2023-04-15 14:15:25 -0600

    Version 1

--- textSwapper.py
+++ textSwapper.py
@@ -11,10 +11,12 @@ from bidi.algorithm import get_display
 
 # -*- coding: utf-8 -*-
 
+'''
 text = 'آدرسة'
 reshaped_text = arabic_reshaper.reshape(text)    # correct its shape
 bidi_text = get_display(reshaped_text)
 print(bidi_text)
+'''
 
 import tkinter as tk #GUI
 import subprocess #to handle copy/paste, clip.exe for windows systems, pbcopy for macOS
@@ -137,47 +139,3 @@ rightSaveBtn = tk.Button(master=rightFrame, text="Save Text as File | حفظ ا�
 rightSaveBtn.pack(side=tk.LEFT)
 
 window.mainloop() #<- always needs to be last line of code, runs event handler
-
-'''
-    #add scramble code here
-    #add scramble code here
-    #add scramble code here   
-
-#generate window
-window = tk.Tk()
-window.title("TEXT SCRAMBLER - Now with support for Arabic!")
-
-#left side of window
-leftFrame = tk.Frame(master=window, relief=tk.RAISED, borderwidth=5, bg="LightBlue3")
-leftFrame.pack() #.pack(side=tk.LEFT) for side-by-side frames
-leftLabel = tk.Label(master=leftFrame, text="Original Text")
-leftLabel.pack()
-leftText = tk.Text(master=leftFrame)
-leftText.pack()
-
-leftPasteBtn = tk.Button(master=leftFrame, text="Paste Text", width=10, height=1, bg="gray88", fg="black", command=paste, activebackground="gray88")
-leftPasteBtn.pack(side=tk.LEFT)
-
-leftOpenBtn = tk.Button(master=leftFrame, text="Open File", width=10, height=1, bg="gray88", fg="black", command=open_file, activebackground="gray88")
-leftOpenBtn.pack(side=tk.LEFT)
-
-leftScrambleBtn = tk.Button(master=leftFrame, text="SCRAMBLE", width=10, height=1, bg="gray88", fg="black", command=lambda: scramble(leftText, rightText), activebackground="gray88")
-leftScrambleBtn.pack(side=tk.RIGHT)
-
-
-#right side of window
-rightFrame = tk.Frame(master=window, relief=tk.RAISED, borderwidth=5, bg="LightBlue3")
-rightFrame.pack() #.pack(side=tk.RIGHT) for side-by-side frames
-rightLabel = tk.Label(master=rightFrame, text="SCRAMBLED Text")
-rightLabel.pack()
-rightText = tk.Text(master=rightFrame)
-rightText.pack()
-
-rightCopyBtn = tk.Button(master=rightFrame, text="Select All & Copy", width=15, height=1, bg="gray88", fg="black", command=copy, activebackground="gray88")
-rightCopyBtn.pack(side=tk.LEFT)
-
-rightSaveBtn = tk.Button(master=rightFrame, text="Save Text as File", width=15, height=1, bg="gray88", fg="black", command=save_file, activebackground="gray88")
-rightSaveBtn.pack(side=tk.LEFT)
-
-window.mainloop() #<- always needs to be last line of code, runs event handler
-'''
